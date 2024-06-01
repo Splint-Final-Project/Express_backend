cbc43ce8a210036dc3386bc6b9eada10f8c576ce
whai2 <whai2125@gmail.com>
2024-06-01 20:24:06 +0900

    feat: 피클 생성 연동

--- backend/controllers/pickle.controller.js
+++ backend/controllers/pickle.controller.js
@@ -1,3 +1,69 @@
+import Pickle from "../models/Pickle.model.js";
+
 export const getPickles = async (req, res) => {
-  // const pickles = await Pickle
-}
+  try {
+    // const { pickleType } = req.params;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    
+    const skip = (page - 1) * limit;
+
+    const pickles = await Pickle.find({}, '_id')
+      .skip(skip)
+      .limit(limit);
+
+    const total = await Pickle.countDocuments();
+
+    res.status(200).json({
+      success: true,
+      count: pickles.length,
+      total,
+      page,
+      pages: Math.ceil(total / limit),
+      data: pickles
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  }
+};
+
+export const createPickle = async (req, res) => {
+  try {
+    const {
+      title,
+      content,
+      latitude,
+      longitude,
+      capacity
+    } = req.body;
+    // 현재 사용자가 생성 -> 리더가 됩니다.
+    const leader = req.user._id;
+
+    const newPickle = new Pickle({
+      leader,
+      title,
+      content,
+      viewCount: 0,
+      latitude,
+      longitude,
+      capacity
+    });
+
+    // 데이터베이스에 저장
+    const savedPickle = await newPickle.save();
+
+    res.status(201).json({
+      success: true,
+      data: savedPickle
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  }
+};
--- backend/routes/pickle.routes.js
+++ backend/routes/pickle.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { getPickles, getPicklesOfType } from "../controllers/pickle.controller.js";
+import { getPickles, createPickle } from "../controllers/pickle.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getPickles);
-router.get("/:pickleType", protectRoute, getPicklesOfType);
+router.post("/create", protectRoute, createPickle);
+// router.get("/:pickleType", protectRoute, getPicklesOfType);
 
 export default router;
