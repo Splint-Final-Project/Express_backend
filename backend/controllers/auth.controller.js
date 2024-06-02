ddfc9ec83e1393e673031467ad37fe2af6450d28
vinoankr <vinoankr@gmail.com>
2024-06-02 15:38:44 +0900

    set cookie 없애고 바디로 토큰 넘기기

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,85 +2,89 @@ import bcrypt from "bcryptjs";
 
 import User from "../models/user.model.js";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
+import generateToken from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
     const { email, password, checkPassword } = req.body;
 
     if (password !== checkPassword) {
-      return res.status(400).json({error: "Passwords don't match"});
+      return res.status(400).json({ error: "Passwords don't match" });
     }
 
-    const user = await User.findOne({email});
-    
+    const user = await User.findOne({ email });
+
     if (user) {
-      return res.status(400).json({error: "User already exists"});
+      return res.status(400).json({ error: "User already exists" });
     }
-    
+
     const salt = await bcrypt.genSalt(10); // 해싱을 통해 비밀번호 해싱 추적을 어렵게
     const hashedPassword = await bcrypt.hash(password, salt);
 
     // https://avatar-placeholder.iran.liara.run/
 
-		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${email}`;
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${email}`;
 
-		const newUser = new User({
-			email,
-			password: hashedPassword,
-			// gender,
-			profilePic: boyProfilePic
-		});
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      // gender,
+      profilePic: boyProfilePic,
+    });
 
     if (newUser) {
-			// Generate JWT token here
-			generateTokenAndSetCookie(newUser._id, res);
-			await newUser.save();
+      // Generate JWT token here
+      // generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
 
-			res.status(201).json({
-				_id: newUser._id,
-				email: newUser.email,
-				profilePic: newUser.profilePic,
-			});
-		} else {
-			res.status(400).json({ error: "Invalid user data" });
-		}
-
+      res.status(201).json({
+        message: "회원가입 성공, 추가정보 입력 페이지로 리다이렉트합니다.",
+        token: generateToken(newUser._id),
+        _id: newUser._id,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
   } catch (err) {
     res.status(500).json({ error: "Internal Server Error" });
     console.log("Error in signup controller", err.message);
   }
-}
+};
 
 export const login = async (req, res) => {
-	try {
-		const { email, password } = req.body;
-		const user = await User.findOne({ email });
-		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
 
-		if (!user || !isPasswordCorrect) {
-			return res.status(400).json({ error: "Invalid email or password" });
-		}
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
 
-		const token = generateTokenAndSetCookie(user._id, res);
-
-		res.status(200).json({
-			_id: user._id,
-			email: user.email,
-			profilePic: user.profilePic,
-			token: token,
-		});
-	} catch (error) {
-		console.log("Error in login controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
-	}
+    res.status(200).json({
+      message: "로그인 성공",
+      token: generateToken(newUser._id),
+      _id: newUser._id,
+      email: newUser.email,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
-	try {
-		res.cookie("jwt", "", { maxAge: 0 });
-		res.status(200).json({ message: "Logged out successfully" });
-	} catch (error) {
-		console.log("Error in logout controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
-	}
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,18 +1,18 @@
 import jwt from "jsonwebtoken";
 
-const generateTokenAndSetCookie = (userId, res) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "15d",
-	});
+const generateToken = (userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
 
-	res.cookie("jwt", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-		secure: process.env.NODE_ENV !== "development",
-	});
+  // res.cookie("jwt", token, {
+  //   maxAge: 15 * 24 * 60 * 60 * 1000, // MS
+  //   httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+  //   sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+  //   secure: process.env.NODE_ENV !== "development",
+  // });
 
-	return token;
+  return token;
 };
 
-export default generateTokenAndSetCookie;
+export default generateToken;
