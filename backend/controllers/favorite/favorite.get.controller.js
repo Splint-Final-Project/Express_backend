aa841647ebf20ea1bda12479ee22158faa2ee08c
whai2 <whai2125@gmail.com>
2024-06-08 17:36:32 +0900

    feat: 찜 목록 조회

---
+++ backend/controllers/favorite/favorite.get.controller.js
@@ -0,0 +1,18 @@
+import Favorite from "../../models/favorite.model.js";
+
+export const getFavorites = async (req, res) => {
+  try {
+    const user = req.user._id; // 현재 로그인 사용자
+
+    const favorites = await Favorite.find({
+      userId: user,
+    });
+
+    res.status(201).send.json({ data: favorites });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "찜 목록 조회에 실패했습니다.",
+    });
+  }
+}
--- backend/routes/favorite.routes.js
+++ backend/routes/favorite.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
 
 import { createFavorite } from "../controllers/favorite/favorite.create.controller.js";
+import { getFavorites } from "../controllers/favorite/favorite.get.controller.js";
+
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/", protectRoute, getFavorites);
+
+// 동적 + not get
 router.post("/:pickleId", protectRoute, createFavorite);
 
 export default router;
