4d13717449deb12204d4228eebb2d6a97206420d
whai2 <whai2125@gmail.com>
2024-06-08 17:23:09 +0900

    feat: 찜하기 기능 구현

---
+++ backend/controllers/favorite/favorite.create.controller.js
@@ -0,0 +1,23 @@
+import Favorite from "../../models/favorite.model";
+
+export const createFavorite = async (req, res) => {
+  try {
+    const user = req.user._id; // 현재 로그인 사용자
+    const favoritePickle = req.body.pickleId;
+
+    const newFavorite = new Favorite({
+      userId: user,
+      pickleId: favoritePickle,
+    });
+
+    // 데이터 베이스 저장
+    await newFavorite.save();
+
+    res.status(201).send.json({ message: "해당 피클을 찜했습니다.", data: newFavorite });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "피클 찜하기에 실패했습니다.",
+    });
+  }
+}
