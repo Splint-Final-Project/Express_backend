d468d9865fbb911a29eaecc753ac58b0ea177cee
vinoankr <vinoankr@gmail.com>
2024-06-16 17:34:09 +0900

    공짜 생성

--- backend/controllers/pickle/pickle.create.controller.js
+++ backend/controllers/pickle/pickle.create.controller.js
@@ -71,7 +71,7 @@ export const createPickle = async (req, res) => {
     const newParticipation = new Participation({
       user: req.user._id,
       pickle: newPickle._id,
-      payment_uid: null,
+      payment_uid: "points",
       amount: 0,
       status: "points",
       isLeader: true,
@@ -84,80 +84,80 @@ export const createPickle = async (req, res) => {
     res
       .status(201)
       .json({ message: "Pickle created successfully", pickle: newPickle });
-  }
+  } else {
+    try {
+      // 이미 존재하는 결제정보인지 확인
+      const already = await Participation.find({
+        payment_uid: imp_uid,
+      });
 
-  try {
-    // 이미 존재하는 결제정보인지 확인
-    const already = await Participation.find({
-      payment_uid: imp_uid,
-    });
+      if (already.length > 0) {
+        // await refund(imp_uid);
+        return res.status(400).json({
+          message: "이미 존재하는 결제 정보입니다. 해킹이 의심됩니다.",
+        });
+      }
 
-    if (already.length > 0) {
-      // await refund(imp_uid);
-      return res.status(400).json({
-        message: "이미 존재하는 결제 정보입니다. 해킹이 의심됩니다.",
+      // 결제 정보 단건 불러오기
+      const { payment } = await verify(imp_uid);
+
+      const pickleData = JSON.parse(payment.custom_data);
+
+      // 결제 정보가 없을 경우
+      if (!payment?.amount) {
+        await refund(imp_uid);
+        return res.status(404).json({
+          message: "결제 정보가 존재하지 않습니다. 피클 생성에 실패했습니다.",
+        });
+      }
+
+      //결제 금액 확인
+      if (
+        pickleData.cost - pickleData.discount !== payment.amount ||
+        payment.status !== "paid"
+      ) {
+        await refund(imp_uid);
+        return res.status(400).json({
+          message: "결제에 실패했습니다. 금액 위변조가 의심됩니다.",
+        });
+      }
+
+      console.log(tomorrow);
+
+      // 새로운 피클 생성
+      const newPickle = new Pickle({
+        ...pickleData,
+        // deadLine: tomorrow,
+        viewCount: 0, // 초기 viewCount 설정
+        isCancelled: false,
+      });
+
+      // 데이터베이스에 저장
+      await newPickle.save();
+
+      // 참가자 정보 생성
+      const newParticipation = new Participation({
+        user: req.user._id,
+        pickle: newPickle._id,
+        payment_uid: imp_uid,
+        amount: payment.amount,
+        status: "paid",
+        isLeader: true,
+      });
+
+      await newParticipation.save();
+
+      // 벡터 db에 저장
+      await vectorDataSaver(newPickle);
+      res
+        .status(201)
+        .json({ message: "Pickle created successfully", pickle: newPickle });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        error: "Server Error",
       });
     }
-
-    // 결제 정보 단건 불러오기
-    const { payment } = await verify(imp_uid);
-
-    const pickleData = JSON.parse(payment.custom_data);
-
-    // 결제 정보가 없을 경우
-    if (!payment?.amount) {
-      await refund(imp_uid);
-      return res.status(404).json({
-        message: "결제 정보가 존재하지 않습니다. 피클 생성에 실패했습니다.",
-      });
-    }
-
-    //결제 금액 확인
-    if (
-      pickleData.cost - pickleData.discount !== payment.amount ||
-      payment.status !== "paid"
-    ) {
-      await refund(imp_uid);
-      return res.status(400).json({
-        message: "결제에 실패했습니다. 금액 위변조가 의심됩니다.",
-      });
-    }
-
-    console.log(tomorrow);
-
-    // 새로운 피클 생성
-    const newPickle = new Pickle({
-      ...pickleData,
-      // deadLine: tomorrow,
-      viewCount: 0, // 초기 viewCount 설정
-      isCancelled: false,
-    });
-
-    // 데이터베이스에 저장
-    await newPickle.save();
-
-    // 참가자 정보 생성
-    const newParticipation = new Participation({
-      user: req.user._id,
-      pickle: newPickle._id,
-      payment_uid: imp_uid,
-      amount: payment.amount,
-      status: "paid",
-      isLeader: true,
-    });
-
-    await newParticipation.save();
-
-    // 벡터 db에 저장
-    await vectorDataSaver(newPickle);
-    res
-      .status(201)
-      .json({ message: "Pickle created successfully", pickle: newPickle });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      success: false,
-      error: "Server Error",
-    });
   }
 };
--- backend/controllers/pickle/pickle.get.controller.js
+++ backend/controllers/pickle/pickle.get.controller.js
@@ -2,27 +2,42 @@
 import Pickle from "../../models/Pickle.model.js";
 import Participation from "../../models/participation.model.js";
 import Favorite from "../../models/favorite.model.js";
-import { findParticipationNumber, addParticipantNumber } from "../services/utils/index.js";
+import {
+  findParticipationNumber,
+  addParticipantNumber,
+} from "../services/utils/index.js";
 
 export const getPickleDetails = async (req, res) => {
   try {
-    const pickle = await Pickle.findById(req.params.id).exec();
+    const pickle = await Pickle.findOne({
+      _id: req.params.id,
+      isCancelled: false,
+    }).exec();
     const participants = await Participation.find({
       pickle: req.params.id,
-      status: 'paid',
-    }).populate('pickle');
-    const leaders = participants.filter(participant => participant.isLeader);
-    const likeCount = await Favorite.countDocuments({ pickleId: req.params.id});
+    }).populate("pickle");
+    const leaders = participants.filter((participant) => participant.isLeader);
+    const likeCount = await Favorite.countDocuments({
+      pickleId: req.params.id,
+    });
 
     if (!pickle) {
       return res.status(404).json({ error: "Pickle not found" });
     }
     const participantNumber = await findParticipationNumber(req.params.id);
-    const picklesWithParticipant = await addParticipantNumber(pickle, participantNumber);
+    const picklesWithParticipant = await addParticipantNumber(
+      pickle,
+      participantNumber
+    );
 
-    const addLikeAndParticipants = { ...picklesWithParticipant, like: likeCount, participantNumber: participants.length, leader: leaders[0].user}
+    const addLikeAndParticipants = {
+      ...picklesWithParticipant,
+      like: likeCount,
+      participantNumber: participants.length,
+      leader: leaders[0].user,
+    };
 
-    res.json({data: addLikeAndParticipants}); // status 필드가 JSON 응답에 포함됩니다.
+    res.json({ data: addLikeAndParticipants }); // status 필드가 JSON 응답에 포함됩니다.
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
