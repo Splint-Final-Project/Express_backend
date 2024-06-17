c9f8692004a2bc576cedcc37919660ae55369f8e
whai2 <whai2125@gmail.com>
2024-06-17 17:26:29 +0900

    feat: dev 환경에서 활성화를 위해 none으로 수정

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,8 +7,9 @@ const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-    // httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: "none", // for dev
+    // sameSite: "sameSite", // CSRF attacks cross-site request forgery attacks
     // secure: process.env.NODE_ENV !== "development",
   });
 
