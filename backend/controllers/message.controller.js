d506acc4b55bdb85436975de78851c8a80cd9a0a
whai2 <whai2125@gmail.com>
2024-06-19 17:07:23 +0900

    feat: 채팅방 id 에 따라 참가자 전원 소켓 id 배정

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,8 +1,44 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-import { getReceiverSocketId, io } from "../socket/socket.js";
+import { getReceiverSocketId, io, getReceiverSocketIds } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
+	try {
+		const { message } = req.body;
+		const { conversationId } = req.params; // params: "send/:id" routes에서
+		const senderId = req.user._id; // 로그인 상태에서 존재함
+
+		const conversation = await Conversation.findOne({_id: conversationId}).populate("messages");
+
+		const newMessage = new Message({
+			senderId,
+			message,
+		});
+
+		if (newMessage) {
+			conversation.messages.push(newMessage._id);
+		}
+
+		// this will run in parallel
+		await Promise.all([conversation.save(), newMessage.save()]);
+
+		// SOCKET IO FUNCTIONALITY WILL GO HERE
+		const receiverSocketIds = getReceiverSocketIds(conversation.participants);
+		for (const receiverSocketId of receiverSocketIds) {
+			if (receiverSocketId) {
+				// io.to(<socket_id>).emit() used to send events to specific client
+				io.to(receiverSocketId).emit("newMessage", newMessage);
+			}
+		}
+
+		res.status(201).json(newMessage);
+	} catch (error) {
+		console.log("Error in sendMessage controller: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
+
+export const sendMessageOneToOne = async (req, res) => {
 	try {
 		const { message } = req.body;
 		const { id: receiverId, pickleId } = req.params; // params: "send/:id" routes에서
@@ -11,34 +47,32 @@ export const sendMessage = async (req, res) => {
 		let conversation = await Conversation.findOne({
 			participants: { $all: [senderId, receiverId] },
 			pickleId: pickleId,
+			isGroup: false,
 		});
 
 		if (!conversation) {
 			conversation = await Conversation.create({
 				participants: [senderId, receiverId],
 				pickleId: pickleId,
+				isGroup: false,
 			});
 		}
 
 		const newMessage = new Message({
 			senderId,
-			// receiverId,
 			message,
-			pickleId,
 		});
 
 		if (newMessage) {
 			conversation.messages.push(newMessage._id);
 		}
 
-		// await conversation.save();
-		// await newMessage.save();
-
 		// this will run in parallel
 		await Promise.all([conversation.save(), newMessage.save()]);
 
 		// SOCKET IO FUNCTIONALITY WILL GO HERE
 		const receiverSocketId = getReceiverSocketId(receiverId);
+
 		if (receiverSocketId) {
 			// io.to(<socket_id>).emit() used to send events to specific client
 			io.to(receiverSocketId).emit("newMessage", newMessage);
@@ -51,13 +85,15 @@ export const sendMessage = async (req, res) => {
 	}
 };
 
-export const getMessages = async (req, res) => {
+export const getMessagesInOneToOne = async (req, res) => {
 	try {
-		const { id: userToChatId } = req.params;
+		const { id: userToChatId, pickleId } = req.params;
 		const senderId = req.user._id;
 
 		const conversation = await Conversation.findOne({
 			participants: { $all: [senderId, userToChatId] },
+			pickleId: pickleId,
+			isGroup: false,
 		}).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
 
 		if (!conversation) return res.status(200).json([]);
@@ -70,3 +106,21 @@ export const getMessages = async (req, res) => {
 		res.status(500).json({ error: "Internal server error" });
 	}
 };
+
+export const getMessages = async (req, res) => {
+	try {
+		const { conversationId } = req.params;
+		// const senderId = req.user._id; // do not use
+
+		const conversation = await Conversation.findOne({_id: conversationId}).populate("messages");
+
+		if (!conversation) return res.status(200).json([]);
+
+		const messages = conversation.messages;
+
+		res.status(200).json(messages);
+	} catch (error) {
+		console.log("Error in getMessages controller: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+}
