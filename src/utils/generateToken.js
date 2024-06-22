c20ca46c75195fca38338e0c389292f02e921796
vinoankr <vinoankr@gmail.com>
2024-06-22 23:16:53 +0900

    allow samesite none

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    // sameSite: "none", // for dev
+    sameSite: "none", // for dev
     // sameSite: "sameSite", // CSRF attacks cross-site request forgery attacks
     secure: process.env.NODE_ENV !== "development",
   });
