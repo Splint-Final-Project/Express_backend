87da01baf144cdd03c6354213f44b0817ee3a9e5
whai2 <whai2125@gmail.com>
2024-06-25 03:31:22 +0900

    fix: cors 수정

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "*",
+		origin: "https://pickle-time-frontend.vercel.app",
 	},
 });
 
