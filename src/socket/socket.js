20d727d607e3a74ece76b1e9eb2aa0c519251ec7
whai2 <whai2125@gmail.com>
2024-06-25 02:49:07 +0900

    Feat: 에러 변경

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -45,15 +45,8 @@ io.on("connection", (socket) => {
 	});
 
 	socket.on("connect_error", (err) => {
-		// the reason of the error, for example "xhr poll error"
-		console.log(err.message);
-	
-		// some additional description, for example the status code of the initial HTTP response
-		console.log(err.description);
-	
-		// some additional context, for example the XMLHttpRequest object
-		console.log(err.context);
-	});
+    console.error("Connection error:", err);
+  });
 
   socket.on('error', (err) => {
     console.error('Socket encountered error: ', err.message, 'Closing socket');
