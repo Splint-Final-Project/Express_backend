42c57ab8f864cab8840349cc3a2bba70d3887a3e
whai2 <whai2125@gmail.com>
2024-06-24 22:50:15 +0900

    fix: cors hearder

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: [`${process.env.FRONTEND_URL}`],
+		origin: ["https://pickle-time-frontend.vercel.app"],
 		methods: ["GET", "POST"],
 	},
 });
