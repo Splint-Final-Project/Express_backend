f7e0e0a9f69bde6f5e0cb8aaebd9e9de3c4b9dbf
vinoankr <vinoankr@gmail.com>
2024-06-24 21:39:18 +0900

    debug spotify

--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,16 +1,20 @@
 import express from "express";
 
-import { getMessages, getMessagesInOneToOne, sendMessageOneToOne, sendMessage } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getMessagesInOneToOne,
+  sendMessageOneToOne,
+  sendMessage,
+} from "../controllers/message.controller.js";
 import { spotifyAuth } from "../middleware/spotifyAuth.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.get('/:conversationId',protectRoute, getMessages);
+router.get("/:conversationId", protectRoute, getMessages);
 router.get("/:pickleId/:id", protectRoute, getMessagesInOneToOne);
 
-router.post("/send/:conversationId", protectRoute, spotifyAuth, sendMessage);
-router.post("/send/:pickleId/:id", protectRoute, sendMessageOneToOne); // protectRoute에 의해 
-
+router.post("/send/:conversationId", protectRoute, sendMessage);
+router.post("/send/:pickleId/:id", protectRoute, sendMessageOneToOne); // protectRoute에 의해
 
 export default router;
