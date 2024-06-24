d19d0d8cec92389cf3888ba78b0f9ff3d9e59155
whai2 <whai2125@gmail.com>
2024-06-25 02:45:47 +0900

    Feat: 디버깅 추가

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -45,8 +45,15 @@ io.on("connection", (socket) => {
 	});
 
 	socket.on("connect_error", (err) => {
-    console.error("Connection error:", err);
-  });
+		// the reason of the error, for example "xhr poll error"
+		console.log(err.message);
+	
+		// some additional description, for example the status code of the initial HTTP response
+		console.log(err.description);
+	
+		// some additional context, for example the XMLHttpRequest object
+		console.log(err.context);
+	});
 
   socket.on('error', (err) => {
     console.error('Socket encountered error: ', err.message, 'Closing socket');
